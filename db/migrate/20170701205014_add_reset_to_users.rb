7ad51f474bed65a4f944e824d1aea0c2efef067a
Joshua Wootonn <joshuawootonn@gmail.com>
2017-07-01 17:03:55 -0500

    Remove migration

--- db/migrate/20170701205014_add_reset_to_users.rb
+++ db/migrate/20170701205014_add_reset_to_users.rb
@@ -1,6 +1,6 @@
 class AddResetToUsers < ActiveRecord::Migration[5.1]
   def change
-    remove_column :users, :reset_digest
+    
     add_column :users, :reset_digest, :string
     add_column :users, :reset_sent_at, :datetime
   end
